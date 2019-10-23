effb7ef7aa199bedb7500b8fb0fc738912fd4d6f
Luminous-InfiniTom <35981510+Luminous-InfiniTom@users.noreply.github.com>
2019-10-23 02:45:17 -0500

    Loan next level
    Winning now loads next level

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks;
+    // Start is called before the first frame update
+    SceneLoader sceneloader;
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
