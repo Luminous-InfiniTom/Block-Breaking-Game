15a4a1a0adf7ab70693f90b39b760d943fb1cef2
Luminous-InfiniTom <35981510+Luminous-InfiniTom@users.noreply.github.com>
2019-11-05 00:44:51 -0600

    Display Score
    The current earned score has been set to display

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
 
     [SerializeField] int pointsPerBlockDestoryed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0;
 
-   
-   
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -22,5 +27,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestoryed;
+        scoreText.text = currentScore.ToString();
     }
 }
